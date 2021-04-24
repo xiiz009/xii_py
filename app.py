927665c09571fed6ef74c6c5668ad9d4f12da2b8
xiiz009 <joseph.rxjs.it@gmail.com>
2021-04-24 11:03:09 +0700

    init calculate

--- app.py
+++ app.py
@@ -1,25 +1,120 @@
-import numpy as np
-import imageio
-import cv2
-import scipy.ndimage
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
+from functools import partial
 
-np.seterr(divide='ignore', invalid='ignore')
-img="/home/taiphan/Downloads/n.jpg"
+__version__ = '0.1'
+__author__ = 'Tai Phan'
+__title__ = 'PyCalc'
+ERROR_MSG = 'ERROR'
 
-def grayscale(rgb):
-  return np.dot(rgb[...,:3],[0.299,0.587,0.114])
+class PyCalcUi(QMainWindow):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle(__title__)
+    self.setFixedSize(235, 235)
+    self.generalLayout = QVBoxLayout()
+    self._centralWidget = QWidget(self)
+    self.setCentralWidget(self._centralWidget)
+    self._centralWidget.setLayout(self.generalLayout)
+    self._createDisplay()
+    self._createButtons()
 
-def dodge(front,back):
-  result=front*255/(255-back)
-  result[result>255]=255
-  result[back==255]=255
-  return result.astype('uint8')
+  def _createDisplay(self):
+    self.display = QLineEdit()
+    self.display.setFixedHeight(35)
+    self.display.setAlignment(Qt.AlignRight)
+    self.display.setReadOnly(True)
+    self.generalLayout.addWidget(self.display)
 
-s=imageio.imread(img)
-g=grayscale(s)
-i=255-g
+  def _createButtons(self):
+    self.buttons = {}
+    buttonsLayout = QGridLayout()
+    buttons = {'7': (0, 0),
+                '8': (0, 1),
+                '9': (0, 2),
+                '/': (0, 3),
+                'C': (0, 4),
+                '4': (1, 0),
+                '5': (1, 1),
+                '6': (1, 2),
+                '*': (1, 3),
+                '(': (1, 4),
+                '1': (2, 0),
+                '2': (2, 1),
+                '3': (2, 2),
+                '-': (2, 3),
+                ')': (2, 4),
+                '0': (3, 0),
+                '00': (3, 1),
+                '.': (3, 2),
+                '+': (3, 3),
+                '=': (3, 4),
+              }
+    for btnText, pos in buttons.items():
+      self.buttons[btnText] = QPushButton(btnText)
+      self.buttons[btnText].setFixedSize(40, 40)
+      buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+      self.generalLayout.addLayout(buttonsLayout)
 
-b = scipy.ndimage.filters.gaussian_filter(i,sigma=10)
-r = dodge(b, g)
+  def setDisplayText(self, text):
+    self.display.setText(text)
+    self.display.setFocus()
 
-cv2.imwrite('n.jpg', r)
+  def displayText(self):
+    return self.display.text()
+
+  def clearDisplay(self):
+    self.setDisplayText('')
+
+class PyCalcCtrl:
+  def __init__(self, model, view):
+    self._view = view
+    self.__connectSignals()
+    self._evalute = model
+
+  def _calculateResult(self):
+    result = self._evalute(exression=self._view.displayText())
+    self._view.setDisplayText(result)
+
+  def __buildExpression(self, sub_exp):
+    if self._view.displayText() == ERROR_MSG:
+      self._view.clearDisplay()
+    expression = self._view.displayText() + sub_exp
+    self._view.setDisplayText(expression)
+
+
+  def __connectSignals(self):
+    for btnText, btn in self._view.buttons.items():
+      if btnText not in {'=', 'C'}:
+        btn.clicked.connect(partial(self.__buildExpression, btnText))
+
+    self._view.buttons['='].clicked.connect(self._calculateResult)
+    self._view.display.returnPressed.connect(self._calculateResult)
+    self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+
+
+def evaluteExpression(exression):
+  try:
+    result = str(eval(exression, {}, {}))
+  except Exception:
+    result = ERROR_MSG
+  return result
+
+def main():
+  pycalc = QApplication(sys.argv)
+  view = PyCalcUi()
+  view.show()
+  model = evaluteExpression
+  PyCalcCtrl(model=model, view=view)
+  sys.exit(pycalc.exec_())
+
+
+if __name__ == '__main__':
+  main()
